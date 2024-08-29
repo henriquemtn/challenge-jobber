bd6546f5d95b8f44c94433018d5bbcea062b28a8
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-08-29 01:18:22 -0300

    fix: image pathing not allowing

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['localhost'], // Permite imagens do localhost
+    },
+  };
 
 export default nextConfig;
