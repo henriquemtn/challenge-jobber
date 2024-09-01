35ad23df925c1685944ad27c27728efd63b00468
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-01 02:01:23 -0300

    delete: searchInput from header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import UserIdent from "./user_ident";
-import { Input } from "./ui/input";
 
 export default function Header() {
   return (
     <div className="fixed top-0 z-10 bg-white w-full h-24 shadow-md">
       <div className="flex h-full justify-between items-center px-4 max-w-[1292px] mx-auto">
         <UserIdent />
-        <Input placeholder="Pesquise por um Job.." className="hidden md:block md:w-2/3" />
       </div>
     </div>
   );
