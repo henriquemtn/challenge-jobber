89a21ff56607a1de1e770734b7d68c79ba2d8a8d
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-01 17:46:48 -0300

    update: model.py and serializers.py

--- backend/djangoapp/tasks/models.py
+++ backend/djangoapp/tasks/models.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User  # Importa o modelo de usuário padr
 
 # Modelo das Task
 class Task(models.Model):
-    title = models.CharField(max_length=255) # Titulo da tarefa
+    title = models.CharField(max_length=255) # Titulo do Job
     created_at = models.DateTimeField(auto_now_add=True) # Data de criação
-    description = models.TextField() # Descricao da tarefa 
-    image = models.ImageField(upload_to='job_images/', blank=True, null=True) # Imagem da tarefa
+    description = models.TextField() # Descricao do Job 
+    image = models.ImageField(upload_to='job_images/', blank=True, null=True) # Imagem do Job
     due_date = models.DateField(blank=True, null=True) # Prazo de entrega
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos') # Usuario que criou a tarefa
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos') # Usuario que criou o Job
+    priority = models.CharField(max_length=255, default='Normal')  # Prioridade do Job 
+    status = models.CharField(max_length=255, default='Todo')  # Status do Job
 
     def __str__(self):
         return self.title
