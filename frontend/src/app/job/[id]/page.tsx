eaed48cb5559dfdebf212a4db07887aee52ade77
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-01 01:08:02 -0300

    add: jobs page

--- frontend/src/app/job/[id]/page.tsx
+++ frontend/src/app/job/[id]/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
-import { Skeleton } from "@/components/ui/skeleton";
+import Header from "@/components/header";
+import DueTime from "@/components/tasks/due-time";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { Task } from "@/types/types";
 import axios from "axios";
+import { FileImage } from "lucide-react";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 export default function JobPage({ params }: any) {
@@ -27,38 +38,72 @@ export default function JobPage({ params }: any) {
   if (!task) {
     return (
       <div className="flex flex-wrap gap-4">
-        <div className="flex flex-col items-center gap-2">
-          <Skeleton className="w-[160px] h-[260px] rounded-md" />
-          <Skeleton className="w-[160px] h-[14px] rounded-md" />
-          <Skeleton className="w-[160px] h-[12px] rounded-md" />
-        </div>
+        <div className="flex flex-col items-center gap-2">Not found</div>
       </div>
     );
   }
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Tasks</h1>
-      <div className="space-y-4">
-        <div key={task.id} className="border p-4 rounded-md shadow-md">
-          <h2 className="font-bold text-lg">Título: {task.title}</h2>
-          <p className="text-sm text-gray-600 mt-2">
-            Descrição: {task.description}
-          </p>
-          <p className="text-sm text-gray-500 mt-1">
-            Data de Criação:{" "}
-            {task.created_at
-              ? new Date(task.created_at).toLocaleDateString()
-              : "N/A"}
-          </p>
-          <p className="text-sm text-gray-500 mt-1">
-            Data de Vencimento:{" "}
-            {task.due_date
-              ? new Date(task.due_date).toLocaleDateString()
-              : "N/A"}
-          </p>
+    <>
+      <Header />
+      <div className="max-w-[1292px] mx-auto mt-4 px-4">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Jobs</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{task.title}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
+        <div className="flex flex-col md:flex-row gap-4  border-[1px] rounded-md mt-4 p-4">
+          {task.image ? (
+            <div className="w-[250px] h-[250px] overflow-hidden">
+              <Image
+                src={task.image}
+                alt="Imagem da tarefa"
+                width={250}
+                height={250}
+                className="object-cover w-full h-full rounded-md"
+              />
+            </div>
+          ) : (
+            <div className="h-[200px] w-full bg-gray-200 flex items-center justify-center rounded-t-md">
+              <FileImage size={32} />
+            </div>
+          )}
+          <div key={task.id} className="px-4 flex flex-col justify-between">
+            <h2 className="font-bold text-lg">Título: {task.title}</h2>
+            <p className="text-sm text-gray-600 mt-2 min-h-[80px]">
+              Descrição: {task.description}
+            </p>
+            <div>
+              <p className="text-sm text-gray-500 mt-1">
+                Data de Criação:{" "}
+                {task.created_at
+                  ? new Date(task.created_at).toLocaleDateString()
+                  : "N/A"}
+              </p>
+              <div className="flex gap-2">
+                <p className="text-sm text-gray-500 mt-1">
+                  Data de Vencimento:{" "}
+                  {task.due_date
+                    ? new Date(task.due_date).toLocaleDateString()
+                    : "N/A"}
+                </p>
+                <DueTime due_date={task.due_date} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
