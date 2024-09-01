88dd56e4e1e28f2ee220293d15404f6989632ec0
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-01 02:10:41 -0300

    remove: unusual callings components in page.tsx

--- frontend/src/app/job/[id]/page.tsx
+++ frontend/src/app/job/[id]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Header from "@/components/header";
-import DueTime from "@/components/tasks/due-time";
+import DeleteTask from "@/components/tasks/delete-task";
+import UpdateTask from "@/components/tasks/edit-task";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -10,9 +11,16 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Task } from "@/types/types";
 import axios from "axios";
-import { FileImage } from "lucide-react";
+import { FileImage, MoreHorizontal } from "lucide-react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
@@ -64,26 +72,36 @@ export default function JobPage({ params }: any) {
         </Breadcrumb>
 
         <div className="flex flex-col md:flex-row gap-4  border-[1px] rounded-md mt-4 p-4">
-          {task.image ? (
-            <div className="w-[250px] h-[250px] overflow-hidden">
-              <Image
-                src={task.image}
-                alt="Imagem da tarefa"
-                width={250}
-                height={250}
-                className="object-cover w-full h-full rounded-md"
-              />
+          <div className="flex flex-col">
+            {task.image ? (
+              <div className="w-full max-w-md h-[250px] overflow-hidden">
+                <Image
+                  src={task.image}
+                  alt="Imagem da tarefa"
+                  width={250}
+                  height={250}
+                  className="object-cover w-full h-full rounded-md"
+                />
+              </div>
+            ) : (
+              <div className="max-h-[28rem] w-full max-w-md  bg-gray-200 flex items-center justify-center rounded-t-md">
+                <FileImage size={32} />
+              </div>
+            )}
+                        <div className="flex flex-row justify-between">
+              <UpdateTask taskId={task.id} />
+              <DeleteTask id={task.id} />
             </div>
-          ) : (
-            <div className="h-[200px] w-full bg-gray-200 flex items-center justify-center rounded-t-md">
-              <FileImage size={32} />
+           
+          </div>
+          <div key={task.id} className="md:px-4 flex flex-col justify-between">
+            <div className="flex flex-col">
+              <h2 className="font-bold text-lg">Título: {task.title}</h2>
+              <p className="text-sm text-gray-600 mt-2 min-h-[80px]">
+                Descrição: {task.description}
+              </p>
             </div>
-          )}
-          <div key={task.id} className="px-4 flex flex-col justify-between">
-            <h2 className="font-bold text-lg">Título: {task.title}</h2>
-            <p className="text-sm text-gray-600 mt-2 min-h-[80px]">
-              Descrição: {task.description}
-            </p>
+            
             <div>
               <p className="text-sm text-gray-500 mt-1">
                 Data de Criação:{" "}
@@ -91,15 +109,12 @@ export default function JobPage({ params }: any) {
                   ? new Date(task.created_at).toLocaleDateString()
                   : "N/A"}
               </p>
-              <div className="flex gap-2">
-                <p className="text-sm text-gray-500 mt-1">
-                  Data de Vencimento:{" "}
-                  {task.due_date
-                    ? new Date(task.due_date).toLocaleDateString()
-                    : "N/A"}
-                </p>
-                <DueTime due_date={task.due_date} />
-              </div>
+              <p className="text-sm text-[#5F33E2] mt-1">
+                Data de Vencimento:{" "}
+                {task.due_date
+                  ? new Date(task.due_date).toLocaleDateString()
+                  : "Nenhuma"}
+              </p>
             </div>
           </div>
         </div>
