0e5d1b1bbf8f55cd27297a12cc46fc2a6af5b381
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-01 21:42:45 -0300

    change: jobs-table.tsx with new design and filters

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/header";
 import JobsDestacados from "@/components/jobs-destacados";
-import JobsTable from "@/components/jobs-table";
+import JobsTable from "@/components/table/jobs-table";
 import React from "react";
 
 export default function Home() {
